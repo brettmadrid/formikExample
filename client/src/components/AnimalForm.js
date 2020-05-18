10dfbe4fc188a99da3240c08d47bf2f5d27c2c41
brettmadrid <brettmadrid9@gmail.com>
2020-05-17 21:09:07 -0700

    completed formik form

--- client/src/components/AnimalForm.js
+++ client/src/components/AnimalForm.js
@@ -1,41 +1,44 @@
 import React, { useState } from 'react'
-import { withFormik } from 'formik'
+import { withFormik, Form, Field } from 'formik'
 
-const AnimalForm = props => {
-  const handleSubmit = event => {
-    event.preventDefault()
-  }
-
+const AnimalForm = ({ values }) => {
   return (
     <div className='animal-form'>
-      <form onSubmit={handleSubmit}>
+      <Form>
         <label>
           Species:
-          <input
-            type='text'
-            name='species'
-            value={props.values.species}
-            onChange={props.handleChange}
-          />
+          <Field type='text' name='species' placeholder='species' />
         </label>
         <label>
           Size:
-          <input
-            type='text'
-            name='size'
-            value={props.values.size}
-            onChange={props.handleChange}
-          />
+          <Field type='text' name='size' placeholder='size' />
+        </label>
+        <label>
+          Diet:
+          <Field component='select' name='diet' className='food-select'>
+            <option>Choose an Option</option>
+            <option value='carnivore'>Carnivore</option>
+            <option value='herbivore'>Herbivore</option>
+            <option value='omnivore'>Omnivore</option>
+          </Field>
+        </label>
+        <label className='checkbox-container'>
+          vaccinations
+          <Field type='checkbox' name='vaccinations' />
+          <span className='checkmark' />
         </label>
         <button type='button'>Anything!</button>
-      </form>
+      </Form>
     </div>
   )
 }
 
 export default withFormik({
+  // may or may not receive props
   mapPropsToValues: props => ({
     species: props.species || '',
     size: '',
+    diet: '',
+    vaccinations: false,
   }),
 })(AnimalForm)
